d5774581a130641c4196e7abac383ed78665494d
Marcus Asplund <marcus@greatname.se>
2018-09-18 12:52:01 +0200

    :star: :whale: deps update

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'globDirectory': 'dist/',
+  'globDirectory': 'build/',
   'swDest': 'build/sw.js',
-  'navigateFallback': 'dist/index.html',
+  'navigateFallback': 'build/index.html',
   'clientsClaim': true,
   'skipWaiting': true
 }
